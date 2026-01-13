1433f86cfc61b6f7a1d9b62065334e2bdd33b8f6
davidumunna <chimarokeumunn98@gmail.com>
2026-01-13 22:59:35 +0000

    fix: correct environment variable name for MongoDB URI

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const { logger } = require('../utils/logger');
 
 async function connectMongo() {
-  const uri = process.env.MONGODB_URI;
+  const uri = process.env.MONGO_URI;
   if (!uri) throw new Error('MONGODB_URI is not set');
-  console.log('MONGODB_URI:', process.env.MONGODB_URI);
+  console.log('MONGODB_URI:', process.env.MONGO_URI);
 
 
   mongoose.set('strictQuery', true);
