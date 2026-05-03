4b31056e422b7b54736f80de537bd66a64e7a440
chima1968 <chimarokeumunna98@gmail.com>
2026-05-03 18:41:32 +0100

    feat: add production client origin to CORS configuration

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ const origins=[process.env.CLIENT_ORIGIN,
   'http://localhost:5173',
   'http://localhost:3000',
   'https://app.hispresencenewcastle.org.uk',
+  'https://hpnnginxgateway-production.up.railway.app',
   "https://hpn-admin-production.up.railway.app",
   'http://192.168.113.206:4000'].filter(Boolean);
 app.use(
